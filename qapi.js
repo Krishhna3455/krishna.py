ef619a288375c6acea0b38c565f19daa91e8e275
krishnalokhande <72542301+Krishhna3455@users.noreply.github.com>
2021-05-06 09:43:25 +0530

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60936c5f765ed9001e2a6cba",
+				"60936c5f765ed9001e2a6cbc"
+			]
+		},
+		"60936c5f765ed9001e2a6cba": {
+			"id": "60936c5f765ed9001e2a6cba",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60936c5f765ed9001e2a6cbc": {
+			"id": "60936c5f765ed9001e2a6cbc",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
